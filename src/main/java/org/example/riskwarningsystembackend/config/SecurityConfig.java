067497ed185ab4f8c07e2d927d08ae7caa26a342
ManeCheung <manecheung@outlook.com>
2025-08-13 11:04:48 +0800

    初始化

--- src/main/java/org/example/riskwarningsystembackend/config/SecurityConfig.java
+++ src/main/java/org/example/riskwarningsystembackend/config/SecurityConfig.java
@@ -3,8 +3,6 @@ package org.example.riskwarningsystembackend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.example.riskwarningsystembackend.security.JwtAuthenticationFilter;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
